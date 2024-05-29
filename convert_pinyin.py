0e1353c5d8b18693316e3bfdb24b702b447ff547
lacie <26832018+aka-lacie@users.noreply.github.com>
2024-05-28 20:30:45 -0700

    Initial commit

---
+++ convert_pinyin.py
@@ -0,0 +1,24 @@
+# https://stackoverflow.com/a/21488584
+
+import re
+
+pinyinToneMarks = {
+    u'a': u'āáǎà', u'e': u'ēéěè', u'i': u'īíǐì',
+    u'o': u'ōóǒò', u'u': u'ūúǔù', u'ü': u'ǖǘǚǜ',
+    u'A': u'ĀÁǍÀ', u'E': u'ĒÉĚÈ', u'I': u'ĪÍǏÌ',
+    u'O': u'ŌÓǑÒ', u'U': u'ŪÚǓÙ', u'Ü': u'ǕǗǙǛ'
+}
+
+def convertPinyinCallback(m):
+    tone=int(m.group(3))%5
+    r=m.group(1).replace(u'v', u'ü').replace(u'V', u'Ü')
+    # for multple vowels, use first one if it is a/e/o, otherwise use second one
+    pos=0
+    if len(r)>1 and not r[0] in 'aeoAEO':
+        pos=1
+    if tone != 0:
+        r=r[0:pos]+pinyinToneMarks[r[pos]][tone-1]+r[pos+1:]
+    return r+m.group(2)
+
+def convertPinyin(s):
+    return re.sub(r'([aeiouüvÜ]{1,3})(n?g?r?)([012345])', convertPinyinCallback, s, flags=re.IGNORECASE)
