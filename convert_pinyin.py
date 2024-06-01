75834b13dd0d721f9889324eab5387b26889ee35
aka-lacie <freyaz495@gmail.com>
2024-06-01 01:23:06 -0700

    Improve vocab card UI

--- convert_pinyin.py
+++ convert_pinyin.py
@@ -9,11 +9,6 @@ pinyinToneMarks = {
     u'O': u'ŌÓǑÒ', u'U': u'ŪÚǓÙ', u'Ü': u'ǕǗǙǛ'
 }
 
-intonated_u_mapping = {
-    'u:1': 'ǖ', 'u:2': 'ǘ', 'u:3': 'ǚ', 'u:4': 'ǜ',
-    'U:1': 'Ǖ', 'U:2': 'Ǘ', 'U:3': 'Ǚ', 'U:4': 'Ǜ'
-}
-
 def convertPinyinCallback(m):
     tone=int(m.group(3))%5
     r=m.group(1).replace(u'v', u'ü').replace(u'V', u'Ü')
@@ -26,5 +21,5 @@ def convertPinyinCallback(m):
     return r+m.group(2)
 
 def convertPinyin(s):
-    s = ''.join(intonated_u_mapping.get(char, char) for char in s)
+    s = s.replace('u:', 'ü').replace('U:', 'Ü')
     return re.sub(r'([aeiouüvÜ]{1,3})(n?g?r?)([012345])', convertPinyinCallback, s, flags=re.IGNORECASE)
